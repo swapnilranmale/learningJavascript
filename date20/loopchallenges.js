be34a3d7092e1e1301d8272635504ecb08f888d7
swapnilranmale <ranmaleswapnil@thinkitive.com>
2024-10-20 21:09:53 +0530

    array using for loop

--- date20/loopchallenges.js
+++ date20/loopchallenges.js
@@ -7,13 +7,34 @@ while (j >= 1) {
 // console.log(countdown);
 
 // challenges 2
-let teaCollection = [];
-let tea;
+// let teaCollection = [];
+// let tea;
+// do {
+//   tea = prompt(`Enter tea`);
+//   if (tea != "stop") {
+//     teaCollection.push(tea);
+//   }
+// } while (tea != "stop");
+
+let num = 1,
+  total = 0;
 do {
-  tea = prompt(`Enter tea`);
-  if (tea != "stop") {
-    teaCollection.push(tea);
-  }
-} while (tea != "stop");
+  total += num;
+  num++;
+} while (num <= 3);
+// console.log(total);
 
+let array = [2, 4, 6];
+let mulArray = [];
+array.map((element) => {
+  mulArray.push(element * 2);
+});
+// console.log(mulArray);
 
+let arrayOfCity = ["Paris", "New York", "London"],
+  cityList = [];
+for (let i = 0; i < arrayOfCity.length; i++) {
+  cityList.push(arrayOfCity[i]);
+}
+console.log("CityList", cityList);
+console.log("ArrayOfCity", arrayOfCity);
